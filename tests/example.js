a0a4c2056e63bec51635fee76de99304fcdb1330
brandontheturner <brandontheturner@gmail.com>
2020-05-13 17:29:44 -0500

    first tests

--- tests/example.js
+++ tests/example.js
@@ -1,5 +1,32 @@
 describe('First Steps with WebdriverIO', () => {
     it('Load Example Website', () => {
-        browser.url('https://paycove.io/');
+        browser.url('http://www.example.com/');
+        browser.pause(2000);
+        expect(browser).toHaveUrl('http://www.example.com/');
+        expect(browser).toHaveTitle('Example Domain');
+    })
+
+    it('h1 should be visible', () => {
+        const h1 = $('h1');
+        h1.waitForExist();
+        expect(h1).toBeVisible();
+    })
+
+    it('p tag should be visible', () => {
+        const p = $('p');
+        p.waitForExist();
+        expect(p).toBeVisible();
+    })
+
+    it('Check link value', () => {
+        const link = $('a');
+        expect(link).toHaveLink('https://www.iana.org/domains/example');
+    }) 
+
+    it('get element text', () => {
+        const text = $('h1').getText();
+        const element = $('h1');
+        element.waitForExist();
+        expect(element).toHaveText('Example Domain');
     })
 })
